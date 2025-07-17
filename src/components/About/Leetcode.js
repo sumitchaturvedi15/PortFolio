9388c4977ce79a3021ef6b4c5d8a4180b68c590f
Sumit Chaturvedi <sumitchaturvedilucky2002@gmail.com>
2025-07-17 23:10:43 +0530

    Update and fixes

--- src/components/About/Leetcode.js
+++ src/components/About/Leetcode.js
@@ -1,35 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 function Leetcode() {
-  const [, setRating] = useState(null);
-  // const [rating, setRating] = useState(null);
   const username = 'Akiraa15';
-
-  useEffect(() => {
-    const fetchRating = async () => {
-      const query = `
-        query getUserContestRanking($username: String!) {
-          userContestRanking(username: $username) {
-            rating
-          }
-        }
-      `;
-      const variables = { username };
-      const response = await fetch('https://leetcode.com/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query, variables }),
-      });
-      const data = await response.json();
-      console.log(data,'dd')
-      setRating(data.data.userContestRanking?.rating || 'N/A');
-    };
-
-    fetchRating();
-  }, [username]);
 
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
@@ -46,14 +19,6 @@ function Leetcode() {
           style={{ width: '100%', maxWidth: '500px', borderRadius: '10px' }}
         />
       </Col>
-
-      {/* LeetCode Contest Rating
-      <Col xs={12} md={8} style={{ textAlign: 'center' }}>
-        <h2 className="purple" style={{ paddingBottom: '10px' }}>⭐ Current Rating</h2>
-        <p style={{ fontSize: '24px', color: '#E63946' }}>
-          {rating !== null ? rating : 'Loading...'}
-        </p>
-      </Col> */}
     </Row>
   );
 }
